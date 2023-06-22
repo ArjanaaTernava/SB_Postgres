142b7c390d12c4eddf6496ed87078a0b913a9dc2
ArjanaaTernava <arjana.ternava@evonem.com>
2023-06-22 16:23:27 +0200

    Creating students with faker generator

--- src/main/java/com/sbproject2/sbproject2/Sbproject2Application.java
+++ src/main/java/com/sbproject2/sbproject2/Sbproject2Application.java
@@ -1,9 +1,11 @@
 package com.sbproject2.sbproject2;
 
+import com.github.javafaker.Faker;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.Sort;
 
 import java.util.List;
 
@@ -17,59 +19,25 @@ public class Sbproject2Application {
     @Bean
     CommandLineRunner commandLineRunner(StudentRepository studentRepository){
         return args -> {
-          Student john = new Student(
-                  "John","Doe","john@doe.com",21
-          );
-          Student jane = new Student("Jane","Doe","jane@doe.com",22);
-            System.out.println("Adding jane and jon to the database");
+            extracted(studentRepository);
 
-            Student travolta = new Student(
-                    "John", "Travolta","john@travolta.com",25
-            );
-          studentRepository.saveAll(List.of(john,jane,travolta));
+            Sort sort = Sort.by("firstName").ascending().and(Sort.by("age").descending());
 
-            System.out.println("fetching a student by email");
-            studentRepository.findStudentByEmail("john@doe.com").ifPresentOrElse(
-                    student -> System.out.println("found " + student), () -> System.out.println("not foouund")
-            );
-
-            System.out.println("fetching students by first name and age");
-            studentRepository.findStudentsByFirstNameEqualsAndAgeEquals("John",21).forEach(System.out::println);
-
-            studentRepository.findStudentsByFirstNameEqualsAndAgeGreaterThan("John",21).forEach(System.out::println);
-
-            studentRepository.findStudentsByFirstNameEqualsAndAgeEqualsNative("John",21).forEach(System.out::println);
-            System.out.println("deleting student by id");
-            studentRepository.deleteStudentById(1L);
-
-//            System.out.println("checking the amount of students in the db");
-//            System.out.println("we have "+ studentRepository.count() + " students in the db");
-//
-//            System.out.println("Checking for student with id 2 in the db");
-//            studentRepository.findById(2L).ifPresentOrElse(student -> {
-//                System.out.println("we found " + student + " in the db");
-//            }, () -> {
-//                System.out.println("we did not find a student with that id");
-//            });
-//
-//            System.out.println("Checking for student with id 3 in the db");
-//            studentRepository.findById(3L).ifPresentOrElse(student -> {
-//                System.out.println("we found " + student + " in the db");
-//            }, () -> {
-//                System.out.println("we did not find a student with that id");
-//            });
-//
-//            System.out.println("fetching all students from the database");
-//            List<Student>students = studentRepository.findAll();
-//            students.forEach(System.out::println);
-//
-//            System.out.println("Deleting student with id 2");
-//
-//            studentRepository.deleteById(2L);
-//
-//            System.out.println("check the amount of students in the db, after deleting student with id 2");
-//            System.out.println("we have "+ studentRepository.count() + " student in the db.");
+            studentRepository.findAll(sort).forEach(student -> System.out.println(student.getFirstName() + " " + student.getAge()));
 
         };
+    }
+
+    private static void extracted(StudentRepository studentRepository) {
+        Faker faker = new Faker();
+
+        for (int i =0; i < 20 ; i++){
+            String firstName = faker.name().firstName();
+            String lastName = faker.name().lastName();
+            String email = String.format("%s.%s@cdp.com",firstName,lastName);
+            int age = faker.number().numberBetween(18,50);
+            Student student = new Student(firstName,lastName,email,age);
+            studentRepository.save(student);
+        }
     }
 }
