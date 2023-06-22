edf7d2b50f66a482d75a2798bbf4407c4e757aad
ArjanaaTernava <arjana.ternava@evonem.com>
2023-06-22 14:19:42 +0200

    Init commit

---
+++ src/main/java/com/sbproject2/sbproject2/Sbproject2Application.java
@@ -0,0 +1,13 @@
+package com.sbproject2.sbproject2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sbproject2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sbproject2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/sbproject2/sbproject2/Sbproject2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sbproject2.sbproject2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sbproject2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
